580b013f48bee57c494d1e06eedef0b0d3a48ef9
yuanjunliang <17602153700@163.com>
2019-09-05 08:49:53 +0800

    first commit

---
+++ app/src/main/java/github/ll/emoticon/common/widget/AnimEmoticonsIndicatorView.java
@@ -0,0 +1,50 @@
+package github.ll.emoticon.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+
+import github.ll.emotionboard.data.EmoticonPack;
+import github.ll.emotionboard.widget.EmoticonsIndicatorView;
+
+/**
+ * 动画的指示器
+ */
+public class AnimEmoticonsIndicatorView extends EmoticonsIndicatorView {
+
+    protected AnimatorSet mPlayToAnimatorSet;
+
+    public AnimEmoticonsIndicatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void playTo(int position, EmoticonPack pack) {
+        if (!checkPack(pack)) {
+            return;
+        }
+
+        updateIndicatorCount(pack.getPageCount());
+
+        for (ImageView iv : mImageViews) {
+            iv.setImageDrawable(mDrawableNormal);
+        }
+        mImageViews.get(position).setImageDrawable(mDrawableSelect);
+        final ImageView imageViewStrat = mImageViews.get(position);
+        ObjectAnimator animIn1 = ObjectAnimator.ofFloat(imageViewStrat, "scaleX", 0.25f, 1.0f);
+        ObjectAnimator animIn2 = ObjectAnimator.ofFloat(imageViewStrat, "scaleY", 0.25f, 1.0f);
+
+        if (mPlayToAnimatorSet != null && mPlayToAnimatorSet.isRunning()) {
+            mPlayToAnimatorSet.cancel();
+            mPlayToAnimatorSet = null;
+        }
+        mPlayToAnimatorSet = new AnimatorSet();
+        mPlayToAnimatorSet.play(animIn1).with(animIn2);
+        mPlayToAnimatorSet.setDuration(100);
+        mPlayToAnimatorSet.start();
+    }
+}
